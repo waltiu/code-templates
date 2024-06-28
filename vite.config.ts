6e5ea8f985be3c10efc08d6e95c456d43bbe4266
waltiu <waltiu@163.com>
2024-06-28 10:20:57 +0800

    feat: 自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,26 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      imports:['vue','vue-router'],
+      eslintrc: {
+        // 已存在文件设置默认 false，需要更新时再打开，防止每次更新都重新生成
+        enabled: false,
+        // 生成文件地址和名称
+        filepath: './.eslintrc-auto-import.cjs',
+      },
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      resolvers: [ElementPlusResolver()]
+    }),
+    Inspect()
   ],
   resolve: {
     alias: {
